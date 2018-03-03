3a779c8336ceb8a175e65ef71a91a44e4dd6fc88
mouqing jin <296313596@qq.com>
2018-03-02 21:42:04 -0600

    ok

---
+++ ICP4/src/main/scala/jinm.scala
@@ -0,0 +1,53 @@
+import org.apache.spark.mllib.evaluation.{BinaryClassificationMetrics, MulticlassMetrics}
+import org.apache.spark.rdd.RDD
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Mayanka on 14-Jul-15.
+  */
+object jinm {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+    val sc=new SparkContext(sparkConf)
+    val data = sc.textFile("data.txt")
+    val parsedData = data.map(line => {line.split(',')}).map(line=>(line(0).toDouble,line(1).toDouble))
+    //val parseLineToTuple: String => Array[(Double,Double)] = s => s=> s.split(',').map(fields => (fields(2).toDouble,fields(3).toDouble))
+
+    val metrics = new MulticlassMetrics(parsedData)
+    val cfMatrix = metrics.confusionMatrix
+    println(" |=================== Confusion matrix ==========================")
+    println(cfMatrix)
+    println(metrics.fMeasure)
+    println(parsedData)
+    /*val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+    val sc=new SparkContext(sparkConf)
+    val data = sc.textFile("data.txt")*/
+
+
+    printf(
+      s"""
+
+         |=================== Confusion matrix ==========================
+         |          | %-15s                     %-15s
+         |----------+----------------------------------------------------
+         |Actual = 0| %-15f                     %-15f
+         |Actual = 1| %-15f                     %-15f
+         |===============================================================
+         """.stripMargin, "Predicted = 0", "Predicted = 1",
+      cfMatrix.apply(0, 0), cfMatrix.apply(0, 1), cfMatrix.apply(1, 0), cfMatrix.apply(1, 1))
+
+    println("\nACCURACY " + ((cfMatrix(0,0) + cfMatrix(1,1))/(cfMatrix(0,0) + cfMatrix(0,1) + cfMatrix(1,0) + cfMatrix(1,1))))
+    println("\nMisclassification_Rate " + ((cfMatrix(0,1) + cfMatrix(1,0))/(cfMatrix(0,0) + cfMatrix(0,1) + cfMatrix(1,0) + cfMatrix(1,1))))
+    println("\nTrue_Positive_Rate " + ((cfMatrix(0,0))/(cfMatrix(0,0) + cfMatrix(0,1))))
+    println("\nFalse_Positive_Rate " + ((cfMatrix(0,1))/(cfMatrix(1,0) + cfMatrix(1,1))))
+    println("\nSpecificity" + ((cfMatrix(1,1))/(cfMatrix(1,0) + cfMatrix(1,1))))
+    println("\nPrecision" +(cfMatrix(0,0))/(cfMatrix(0,0)+(cfMatrix(1,0))))
+    println("\nPrevalence" + (cfMatrix(0,0) + cfMatrix(0,1))/(cfMatrix(0,0) + cfMatrix(0,1) + cfMatrix(1,0) + cfMatrix(1,1)))
+
+
+
+
+  }
+}
--- ICP7/LinearRegression/LinearRegression.py
+++ ICP7/LinearRegression/LinearRegression.py
@@ -24,9 +24,6 @@ X = tf.placeholder("float")
 
 Y = tf.placeholder("float")
 
-
-
-
 # create a shared variable for the weight matrix
 
 w = tf.Variable(rng.randn(), name="weights")
@@ -35,8 +32,6 @@ b = tf.Variable(rng.randn(), name="bias")
 
 # prediction function
 y_model = tf.add(tf.multiply(X, w), b)
-
-
 # Mean squared error
 
 cost = tf.reduce_sum(tf.pow(y_model-Y, 2))/(2*100)
@@ -44,24 +39,14 @@ cost = tf.reduce_sum(tf.pow(y_model-Y, 2))/(2*100)
 # construct an optimizer to minimize cost and fit line to my data
 
 train_op = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
-
-
-
-
-
-
 # Launch the graph in a session
 sess = tf.Session()
 
 # Initializing the variables
 
 init = tf.global_variables_initializer()
-
-
 # you need to initialize variables
 sess.run(init)
-
-
 for i in range(100):
     for (x, y) in zip(trX, trY):
         sess.run(train_op, feed_dict={X: x, Y: y})
